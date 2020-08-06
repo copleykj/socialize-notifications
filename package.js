5cf65fc6ac5d565aa713ed0afd7b0486dc21e196
Kelly Copley <copleykj@gmail.com>
2020-08-06 16:33:45 -0400

    update versionsFrom to 1.10.2

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
+  api.versionsFrom('1.10.2');
   api.use('ecmascript');
   api.mainModule('notifications.js');
 });
